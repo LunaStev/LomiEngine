feb18673a87b6db193dad38a30404c2359dadf10
LunaStev <quingames3390@gmail.com>
2024-09-05 11:41:54 +0900

    기초 파일

---
+++ include/LomiEngine/Engine.h
@@ -0,0 +1,19 @@
+//
+// Created by HSC on 2024-09-05.
+//
+
+#ifndef ENGINE_H
+#define ENGINE_H
+
+namespace LomiEngine {
+    class Engine {
+    public:
+        Engine();
+        ~Engine();
+
+        void init();
+        void update();
+    };
+}
+
+#endif //ENGINE_H
