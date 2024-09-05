1416820ee2b9720921383c624f21c76f8aa2948f
LunaStev <quingames3390@gmail.com>
2024-09-05 11:42:03 +0900

    기초 파일

---
+++ include/LomiEngine/Renderer.h
@@ -0,0 +1,18 @@
+//
+// Created by HSC on 2024-09-05.
+//
+
+#ifndef RENDERER_H
+#define RENDERER_H
+
+namespace LomiEngine {
+    class Renderer {
+    public:
+        Renderer();
+        ~Renderer();
+
+        void render();
+    };
+}
+
+#endif //RENDERER_H
