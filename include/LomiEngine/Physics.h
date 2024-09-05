516cd8c31afc14a1270607b7cf4fb7c2b674d155
LunaStev <quingames3390@gmail.com>
2024-09-05 11:41:58 +0900

    기초 파일

---
+++ include/LomiEngine/Physics.h
@@ -0,0 +1,18 @@
+//
+// Created by HSC on 2024-09-05.
+//
+
+#ifndef PHYSICS_H
+#define PHYSICS_H
+
+namespace LomiEngine {
+    class Physics {
+    public:
+        Physics();
+        ~Physics();
+
+        void simulate();
+    };
+}
+
+#endif //PHYSICS_H
